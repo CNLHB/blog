7f41f65e92f32564ff5656aa9e037cb43e519f1e
爱华 <1348844909@qq.com>
2025-06-12 12:55:08 +0800

    fix/修复打包base路径

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/blog",
   title: "爱华Fullstack",
   description: "随想录",
   themeConfig: {
